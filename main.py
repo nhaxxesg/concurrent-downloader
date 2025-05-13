efff85284e1b27050ef2e310cbe392fe08cf30b3
badman <josueangelferroquispe@gmail.com>
2025-05-12 19:29:39 -0500

    define downloaders and implement validate url

--- main.py
+++ main.py
@@ -14,13 +14,28 @@ def read_stdin_urls(urls: list[str]):
 def _validate_url():
     ...
 
-async def worker(url: str):
-    print(type(requests.get(url)))
+async def prepare_downloaders(urls: list[str]):
+    # create tasks 
+    for url in urls:
+        _validate_url(url)
+        async def downloader(process_url: str):
+            file = requests.get(process_url)
+        
+        asyncio.create_task(downloader(url))
+            
+async def _validate_url(url: str):
+    if url[3:] == "pdf":
+        return url
+    raise ValueError("Url not supported")
 
 async def main():
-    print("Hello from concurrent-downloader!")
-    result = await worker("https://edu.anarcho-copy.org/Programming%20Languages/Python/using-asyncio-python-understanding-asynchronous.pdf")
-    print(read_url_file("README.md"))
+    # print("Hello from concurrent-downloader!")
+    # result = await worker("https://edu.anarcho-copy.org/Programming%20Languages/Python/using-asyncio-python-understanding-asynchronous.pdf")
+    # print(read_url_file("README.md"))
+    task = asyncio.create_task(worker("https://edu.anarcho-copy.org/Programming%20Languages/Python/using-asyncio-python-understanding-asynchronous.pdf"))
+    await task
+
+    await worker("https://edu.anarcho-copy.org/Programming%20Languages/Python/using-asyncio-python-understanding-asynchronous.pdf")
 
 if __name__ == "__main__":
     asyncio.run(main())
